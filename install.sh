4672c4c58904b7764f8d237aa6b86ab3e272a1bd
zots0127 <zots0127@gmail.com>
2017-07-22 19:47:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ fi
 #Install SSR and SSR-Bash
 cd /usr/local
 git clone https://github.com/shadowsocksr/shadowsocksr.git
-git clone https://github.com/FunctionClub/SSR-Bash-Python.git
+git clone https://github.com/zots0127/SSR-Bash-Python.git
 cd /usr/local/shadowsocksr
 bash initcfg.sh
 
